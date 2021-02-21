0f9c56864a92eabce6c30ece933c73df64a022ca
David Stolarsky <david.stolarsky@gmail.com>
2021-02-21 10:46:24 -0800

    boom

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 const {Command, flags} = require('@oclif/command')
+const {readFile, writeFile} = require('mz/fs')
+const path = require('path')
+const {pascalCase} = require('change-case')
 
 class Svg2ReactCommand extends Command {
   async run() {
     const {args} = this.parse(Svg2ReactCommand)
-    const file = args.file;
-    this.log(`hello let's convert ${file} from ./src/index.js`)
+    const file = args.file
+    const svgString = await readFile(file, {encoding: 'utf8'})
+    const pathParts = path.parse(file)
+    const componentName = pascalCase(pathParts.name)
+    const reactString = `import React from 'react';
+
+const ${componentName} = () => {
+  return ${svgString};
+};
+
+export default ${componentName};
+`
+    const out = path.join(pathParts.dir, componentName + '.js')
+    await writeFile(out, reactString, {encoding: 'utf8'})
+    this.log(`Saved ${out}`)
   }
 }
 
@@ -14,8 +30,8 @@ Extra documentation goes here
 `
 
 Svg2ReactCommand.args = [
-  {name: 'file', required: true}
-];
+  {name: 'file', required: true},
+]
 
 Svg2ReactCommand.flags = {
   // add --version flag to show CLI version
